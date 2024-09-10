f1ecbf6306ca47f1a4009d801e9553a55e601369
PlasmaBeVibin <Jayden.Welch@s.capregboces.org>
2024-09-10 09:55:14 -0400

    Initialize Solution1

---
+++ CreatingVariablesWTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CreatingVariablesWTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Variables
+            string name = "Shadow";
+            string breed = "Golden Retriever";
+            int age = 5;
+            double weight = 65.22;
+
+
+            // Print variables to the console
+            Console.WriteLine(name);
+            Console.WriteLine(breed);
+            Console.WriteLine(age);
+            Console.WriteLine(weight); 
+
+
+        }
+    }
+}
